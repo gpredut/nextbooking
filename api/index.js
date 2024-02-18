fe7f6639a21e08688864832a9c89573d9363f778
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-02-18 18:37:17 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://nextbooking-client.vercel.app/"],
+    origin: ["https://nextbooking-client.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
