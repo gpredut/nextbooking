f48b9a65c2185391ef9876eb8cc399f91312c948
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-02-18 17:03:27 +0200

    Add files via upload

---
+++ api/routes/hotels.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+  getHotelRooms,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+const router = express.Router();
+
+router.post("/", verifyAdmin, createHotel);
+router.put("/:id", verifyAdmin, updateHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
+router.get("/find/:id", getHotel);
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router;
---
+++ api/routes/rooms.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.post("/:hotelid", verifyAdmin, createRoom);
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+router.get("/:id", getRoom);
+router.get("/", getRooms);
+
+export default router;
