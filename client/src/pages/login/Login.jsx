5b284b62b91008ca92f20203bcdc6825f4c98775
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 22:17:24 +0200

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post("https://nextbooking-client.vercel.app/api/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/");
     } catch (err) {
