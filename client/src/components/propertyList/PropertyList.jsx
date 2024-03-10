43ce9b4bfb1525838ad228eef39c576523f9a920
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 19:42:08 +0200

    Update PropertyList.jsx

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,28 +1,30 @@
 import useFetch from "../hooks/useFetch.js";
+import { useEffect, useState } from "react";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  const { data, loading, error } = useFetch(
-    "/api/hotels/countByType"
-  );
+  const { data, loading, error } = useFetch("/api/hotels/countByType");
+  const [images, setImages] = useState([]);
 
-  // Check for errors during data fetching
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  const images = [
-    "https://i.im.ge/2024/02/14/cWpYDS.photo-1618773928121-c32242e63f39q80w2070autoformatfitcropixlibrb-4-0.jpg",
-    "https://i.im.ge/2024/02/14/cWpxey.photo-1560448204-e02f11c3d0e2q80w2070autoformatfitcropixlibrb-4-0.jpg",
-    "https://i.im.ge/2024/02/14/cWpAlx.photo-1592555059503-0a774cb8d477q80w1935autoformatfitcropixlibrb-4-0.jpg",
-    "https://i.im.ge/2024/02/14/cWpP6L.photo-1564013799919-ab600027ffc6q80w2070autoformatfitcropixlibrb-4-0.jpg",
-    "https://i.im.ge/2024/02/14/cWp8SF.photo-1510798831971-661eb04b3739q80w1974autoformatfitcropixlibrb-4-0.jpg",
-  ];
+  useEffect(() => {
+    // Set images once data is fetched
+    if (data) {
+      setImages([
+        "https://i.im.ge/2024/02/14/cWpYDS.photo-1618773928121-c32242e63f39q80w2070autoformatfitcropixlibrb-4-0.jpg",
+        "https://i.im.ge/2024/02/14/cWpxey.photo-1560448204-e02f11c3d0e2q80w2070autoformatfitcropixlibrb-4-0.jpg",
+        "https://i.im.ge/2024/02/14/cWpAlx.photo-1592555059503-0a774cb8d477q80w1935autoformatfitcropixlibrb-4-0.jpg",
+        "https://i.im.ge/2024/02/14/cWpP6L.photo-1564013799919-ab600027ffc6q80w2070autoformatfitcropixlibrb-4-0.jpg",
+        "https://i.im.ge/2024/02/14/cWp8SF.photo-1510798831971-661eb04b3739q80w1974autoformatfitcropixlibrb-4-0.jpg",
+      ]);
+    }
+  }, [data]);
 
   return (
     <div className="pList">
       {loading ? (
         "Loading..."
+      ) : error ? (
+        <div>Error: {error.message}</div>
       ) : (
         <>
           {Array.isArray(data) &&
