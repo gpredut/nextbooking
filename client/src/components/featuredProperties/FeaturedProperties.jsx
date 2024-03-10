16b947873c504d92c8cfb60f15093d239b216897
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 18:31:41 +0200

    Update FeaturedProperties.jsx

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,19 +2,25 @@ import useFetch from "../hooks/useFetch.js";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const { data, loading } = useFetch("/hotels?featured=true&limit=4");
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
-  // Check if data is an array before mapping
-  if (!Array.isArray(data)) {
-    // Render loading message or other UI when data is not available
+  if (loading) {
+    // Render loading message
     return <div>Loading, please wait...</div>;
+  }
+
+  if (error || !Array.isArray(data)) {
+    // Render error message or other UI when data is not available
+    return <div>Error loading properties</div>;
   }
 
   return (
     <div className="fp">
       {data.map((item) => (
         <div className="fpItem" key={item._id}>
-          <img src={item.photos[0]} alt="" className="fpImg" />
+          {item.photos && item.photos.length > 0 && (
+            <img src={item.photos[0]} alt="" className="fpImg" />
+          )}
           <div className="fpInfo">
             <span className="fpName">{item.name}</span>
             <span className="fpCity">{item.city}</span>
