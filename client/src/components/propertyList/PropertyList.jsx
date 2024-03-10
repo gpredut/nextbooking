8ce93a7890b471990f13992b63a94ab69c9f4c41
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 19:05:31 +0200

    Update PropertyList.jsx

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -3,25 +3,29 @@ import useFetch from "../hooks/useFetch.js";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  const { data, loading } = useFetch("https://nextbooking-api.vercel.app/api/hotels/countByType");
+  const { data, loading, error } = useFetch("https://nextbooking-api.vercel.app/api/hotels/countByType");
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    // Ensure all images are loaded before rendering
-    Promise.all([
-      loadImage("https://i.im.ge/2024/02/14/cWpYDS.photo-1618773928121-c32242e63f39q80w2070autoformatfitcropixlibrb-4-0.jpg"),
-      loadImage("https://i.im.ge/2024/02/14/cWpxey.photo-1560448204-e02f11c3d0e2q80w2070autoformatfitcropixlibrb-4-0.jpg"),
-      loadImage("https://i.im.ge/2024/02/14/cWpAlx.photo-1592555059503-0a774cb8d477q80w1935autoformatfitcropixlibrb-4-0.jpg"),
-      loadImage("https://i.im.ge/2024/02/14/cWpP6L.photo-1564013799919-ab600027ffc6q80w2070autoformatfitcropixlibrb-4-0.jpg"),
-      loadImage("https://i.im.ge/2024/02/14/cWp8SF.photo-1510798831971-661eb04b3739q80w1974autoformatfitcropixlibrb-4-0.jpg")
-    ])
-    .then((loadedImages) => {
-      setImages(loadedImages);
-    })
-    .catch((error) => {
-      console.error('Error loading images:', error);
-    });
-  }, []);
+    if (error) {
+      console.error('Failed to fetch data:', error);
+    } else {
+      // Ensure all images are loaded before rendering
+      Promise.all([
+        loadImage("https://i.im.ge/2024/02/14/cWpYDS.photo-1618773928121-c32242e63f39q80w2070autoformatfitcropixlibrb-4-0.jpg"),
+        loadImage("https://i.im.ge/2024/02/14/cWpxey.photo-1560448204-e02f11c3d0e2q80w2070autoformatfitcropixlibrb-4-0.jpg"),
+        loadImage("https://i.im.ge/2024/02/14/cWpAlx.photo-1592555059503-0a774cb8d477q80w1935autoformatfitcropixlibrb-4-0.jpg"),
+        loadImage("https://i.im.ge/2024/02/14/cWpP6L.photo-1564013799919-ab600027ffc6q80w2070autoformatfitcropixlibrb-4-0.jpg"),
+        loadImage("https://i.im.ge/2024/02/14/cWp8SF.photo-1510798831971-661eb04b3739q80w1974autoformatfitcropixlibrb-4-0.jpg")
+      ])
+      .then((loadedImages) => {
+        setImages(loadedImages);
+      })
+      .catch((error) => {
+        console.error('Error loading images:', error);
+      });
+    }
+  }, [error]);
 
   const loadImage = async (url) => {
     try {
