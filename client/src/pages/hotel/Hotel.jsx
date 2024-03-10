3de422df22bc09c716143986cb24c209365212ef
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 20:59:21 +0200

    Update Hotel.jsx

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -24,7 +24,7 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading } = useFetch(`/hotels/find/${id}`);
+  const { data, loading } = useFetch(`https://nextbooking-api.vercel.app/api/hotels/find/${id}`);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
