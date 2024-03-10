eecac3a7ddc54e4a91d74b3a6b2c2bbebfbc88f8
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 20:51:29 +0200

    Update List.jsx

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -18,7 +18,7 @@ const List = () => {
   const [max, setMax] = useState(999);
 
  const { data, loading, reFetch } = useFetch(
-  `https://nextbooking-api.vercel.app/hotels?city=${destination}&min=${min}&max=${max}`
+  `https://nextbooking-api.vercel.app/api/hotels?city=${destination}&min=${min}&max=${max}`
 );
 
   const handleClick = () => {
