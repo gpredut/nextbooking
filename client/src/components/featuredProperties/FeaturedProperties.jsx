43c2950cd6c95699cea507104a82a36bfac2c6a4
Gabriela Preduț <127437147+gpredut@users.noreply.github.com>
2024-03-10 19:19:51 +0200

    Update FeaturedProperties.jsx

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,48 +1,39 @@
-import { useState } from 'react';
 import useFetch from "../hooks/useFetch.js";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const [imagesLoaded, setImagesLoaded] = useState(false);
   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
-  // Function to handle image loading
-  const handleImageLoad = () => {
-    setImagesLoaded(true);
-  };
+  // Check for errors during data fetching
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div className="fp">
       {loading ? (
-        "Loading, please wait..."
-      ) : error ? (
-        `Error fetching data: ${error}`
+        "Loading, please wait"
       ) : (
         <>
-          {data && data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              {/* Check if photos exist */}
-              {item.photos && item.photos.length > 0 ? (
-                <img
-                  src={item.photos[0]}
-                  alt=""
-                  className="fpImg"
-                  onLoad={handleImageLoad} // Add onLoad event handler
-                />
-              ) : (
-                <div className="fpImgPlaceholder" /> // Placeholder if no photo
-              )}
-              <div className="fpInfo">
-                <span className="fpName">{item.name}</span>
-                <span className="fpCity">{item.city}</span>
+          {data ? (
+            data.map((item) => (
+              <div className="fpItem" key={item._id}>
+                {/* Check if photos exist */}
+                {item.photos && item.photos.length > 0 && (
+                  <img src={item.photos[0]} alt="" className="fpImg" />
+                )}
+                <div className="fpInfo">
+                  <span className="fpName">{item.name}</span>
+                  <span className="fpCity">{item.city}</span>
+                </div>
+                <span className="fpPrice">
+                  Starting from ${item.cheapestPrice}
+                </span>
               </div>
-              <span className="fpPrice">
-                Starting from ${item.cheapestPrice}
-              </span>
-            </div>
-          ))}
-          {/* Show loading message if images are still loading */}
-          {!imagesLoaded && <div className="fpLoading">Loading images...</div>}
+            ))
+          ) : (
+            <div>No data available</div>
+          )}
         </>
       )}
     </div>
